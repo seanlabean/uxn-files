5a7e515e2cc9642f4c8481cfce26215d818cf339
seanlabean <sean.phys@gmail.com>
2024-05-19 22:15:25 -0400

    moving heart

--- run-test.sh
+++ run-test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
-rm ../uxn/bin/hello-animate.rom
-/home/seanp/uxn/bin/uxnasm.exe ./testing-tal/hello-animate.tal ../uxn/bin/hello-animate.rom
-/home/seanp/uxn/bin/uxnemu.exe ../uxn/bin/hello-animate.rom
+rm ../uxn/bin/hello-animate-sprite.rom
+/home/seanp/uxn/bin/uxnasm.exe ./testing-tal/hello-animate-sprite.tal ../uxn/bin/hello-animate-sprite.rom
+/home/seanp/uxn/bin/uxnemu.exe ../uxn/bin/hello-animate-sprite.rom
