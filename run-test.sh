722dbbf6ede8ee2f8f80f0341d19fef8af48346f
seanlabean <sean.phys@gmail.com>
2024-06-01 17:50:27 -0400

    hello-pong init commit

--- run-test.sh
+++ run-test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
-rm ../uxn/bin/hello-mouse.rom
-/home/seanp/uxn/bin/uxnasm.exe ./testing-tal/hello-mouse.tal ../uxn/bin/hello-mouse.rom
-/home/seanp/uxn/bin/uxnemu.exe ../uxn/bin/hello-mouse.rom
+rm ../uxn/bin/hello-pong.rom
+/home/seanp/uxn/bin/uxnasm.exe ./testing-tal/hello-pong.tal ../uxn/bin/hello-pong.rom
+/home/seanp/uxn/bin/uxnemu.exe ../uxn/bin/hello-pong.rom
